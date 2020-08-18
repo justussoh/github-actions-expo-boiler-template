ac52177d5a64e684966b29379ba060c1e92ed890
justussoh <justus.soh@hotmail.com>
2020-08-19 00:32:55 +0800

    feat: added linting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "universe/native",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Expo working together with GitHub Actions starter
+
+This is the example explained in a dev.to post, more details will be available soon.
